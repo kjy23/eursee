e2251e044e4953e8d8d545775b69d3da6e013af8
kjy23 <122200448+kjy23@users.noreply.github.com>
2025-05-16 22:45:30 +0800

    Add files via upload

---
+++ eur.py
@@ -0,0 +1,17 @@
+import re
+
+# 读取 Eur.m3u 文件
+with open('Eur.m3u', 'r', encoding='utf-8') as file:
+    m3u_content = file.read()
+
+# 正则表达式，匹配小括号和方括号及其内容并删除
+pattern = r'[\(\[].*?[\)\]]'
+
+# 替换内容，删除方括号和小括号内的部分
+updated_m3u_content = re.sub(pattern, '', m3u_content)
+
+# 将更新后的内容写入到新的文件 eury.m3u
+with open('eury.m3u', 'w', encoding='utf-8') as file:
+    file.write(updated_m3u_content)
+
+print("M3U 文件已更新并保存为 eury.m3u")
